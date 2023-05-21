f2853b575ef0de41f47fadff74051c85a357bc94
Elena <Elena3405674@mail.ru>
2023-05-22 01:44:08 +0300

    feat: using colored numbers

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -3,23 +3,24 @@ const columns = 10;
 
 let board = [];
 
-const minesCount = 5;
+const minesCount = 10;
 const minesLocation = []; //"2-3", "1-5","6-4"
 
-let cellsClicked = 0;
+let countCellsClicked = 0;
 let flagEnabled = false;
 let gameOver = false;
 
 window.onload = function() {
+  draw();
   startGame();
 }
 
 function setMines() {
-  // minesLocation.push('2-2');
-  // minesLocation.push('2-3');
-  // minesLocation.push('5-6');
-  // minesLocation.push('3-4');
   // minesLocation.push('1-1');
+  // minesLocation.push('2-2');
+  // minesLocation.push('3-3');
+  // minesLocation.push('4-4');
+  // minesLocation.push('5-5');
 
   let minesLeft = minesCount;
   while (minesLeft > 0) {
@@ -34,35 +35,45 @@ function setMines() {
   }
 }
 
-
-function startGame () {
+function draw() {
   const container = document.createElement('div');
-  const title = document.createElement('h2');
-  const button = document.createElement('button');
+  const header = document.createElement('header');
+  
+  const buttonSmile = document.createElement('button');
+  const buttonNewGame = document.createElement('button');
   const field = document.createElement('main');
 
-  button.addEventListener('click', onSetSmile);
+  buttonSmile.addEventListener('click', onSetSmile);
+  // buttonNewGame.addEventListener('click', startGame);
   
-  button.textContent = '🙂';
-  title.textContent = "Mines: " + minesCount;
+  buttonSmile.textContent = '🙂';
+  buttonNewGame.textContent = 'New game';
 
   container.classList.add('container');
+  header.classList.add('header');
   field.classList.add('board');
-  title.classList.add('title');
-  button.classList.add('smile-button');
+ 
+  buttonSmile.classList.add('smile-button');
+  buttonNewGame.classList.add('new-game');
 
-  container.append(title, button, field);
+  header.append(buttonNewGame, buttonSmile, );
+  container.append(header, field);
   document.body.prepend(container);
-  
-  setMines();
+}
 
+
+function startGame () {
+  setMines();
+  
   for (let r = 0; r < rows; r++) {
     let row = [];
     for (let c = 0; c < columns; c++) {
       let cell = document.createElement('div');
       cell.classList.add('cell');
-      cell.id = r + '-' + c;
+      cell.id = r.toString() + '-' + c.toString();
       cell.addEventListener('click', onCellClick);
+      const field = document.querySelector('.board');
+
       field.append(cell);
       row.push(cell)
     }
@@ -70,7 +81,7 @@ function startGame () {
   }
   console.log(board);
 }
- 
+
 function onSetSmile() {
   if (flagEnabled) {
     flagEnabled = false;
@@ -83,7 +94,7 @@ function onSetSmile() {
 
 function onCellClick() {
   console.log(event.target.id);
-  let clickedCell = event.target;
+  const clickedCell = event.target;
 
   if (gameOver || clickedCell.classList.contains('cell-checked')) {
     return;
@@ -99,19 +110,30 @@ function onCellClick() {
   }
 
   if (minesLocation.includes(clickedCell.id)) {
-    // alert('GAME OVER');
+    const lose = document.createElement('p');
+    lose.classList.add('lose');
+    lose.textContent = 'Game over. Try again';
+    document.querySelector('.container').append(lose);
     gameOver = true;
     revealMines();
+    document.querySelector('.new-game').addEventListener('click', onRestartGame)
     return;
   }
-
-  let coords = clickedCell.id.split('-');
+  
+  let coords = clickedCell.id.split('-'); // "2-5" => ["2", "5"]
   console.log(coords);
   let r = parseInt(coords[0]);
   let c = parseInt(coords[1]);
   
   checkMine(r, c);
+}
 
+function onRestartGame() {
+  gameOver = false;
+  document.querySelector('.board').innerHTML = '';
+  document.querySelector('.lose').innerHTML = '';
+  
+  startGame();
 }
 
 function revealMines() {
@@ -134,7 +156,9 @@ function checkMine(r, c) {
   if (board[r][c].classList.contains('cell-checked')) {
     return;
   }
+  
   board[r][c].classList.add('cell-checked');
+  countCellsClicked += 1;
 
   let minesFound = 0;
 
@@ -154,25 +178,29 @@ function checkMine(r, c) {
   
   if (minesFound > 0) {
     board[r][c].textContent = minesFound;
+    board[r][c].classList.add("bomb" + minesFound.toString());
   }
   else {
     //top 3 
-    checkMine(r-1, c-1); //top left
-    checkMine(r-1, c); //top
-    checkMine(r-1, c+1); //top right
+    checkCell(r-1, c-1); //top left
+    checkCell(r-1, c); //top
+    checkCell(r-1, c+1); //top right
 
     //left and right
-    checkMine(r, c-1); //left
-    checkMine(r, c+1); //right
+    checkCell(r, c-1); //left
+    checkCell(r, c+1); //right
     
     //bottom 3
-    checkMine(r+1, c-1); //bottom left
-    checkMine(r+1, c); //bottom 
-    checkMine(r+1, c+1); //bottom right
+    checkCell(r+1, c-1); //bottom left
+    checkCell(r+1, c); //bottom 
+    checkCell(r+1, c+1); //bottom right
   }
 
-  if (cellsClicked === rows * columns - minesCount) {
-    title.textContent = 'Game over. Try again';
+  if (countCellsClicked === rows * columns - minesCount) {
+    const won = document.createElement('p');
+    won.classList.add('won');
+    won.textContent = 'You found all mines!!!'
+    document.querySelector('.container').append(won);
     gameOver = true;
   }
 }
@@ -182,7 +210,7 @@ function checkCell(r, c) {
     return 0;
   }
 
-  if (minesLocation.includes(r.toString() + '-' + c.toString())) {
+  if (minesLocation.includes(r + '-' + c)) {
     return 1;
   }
   return 0;
